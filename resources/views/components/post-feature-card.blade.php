819bb6cc6bda4a8920099c8a4b267895e82f8f67
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2022-06-02 21:49:11 +0200

    Added the updateded design

--- resources/views/components/post-feature-card.blade.php
+++ resources/views/components/post-feature-card.blade.php
@@ -32,7 +32,11 @@
 
             <footer class="flex justify-between items-center mt-8">
                 <div class="flex items-center text-sm">
-                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                    </svg>
                     <div class="ml-3">
                         <h5 class="font-bold">
                             <a href="/?author={{ $post->author->username }}">{{ $post->author->name }}</a>
